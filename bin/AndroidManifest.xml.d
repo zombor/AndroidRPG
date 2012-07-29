877eada17010fde056c468c40cb16ab3ddfb569d
Jeremy Bush <contractfrombelow@gmail.com>
2012-07-28 22:17:49 -0500

    remove binfiles

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Volumes/Code/AndroidRPG/bin/AndroidManifest.xml : \
-/Volumes/Code/AndroidRPG/AndroidManifest.xml \
--- bin/AndroidRPG-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Volumes/Code/AndroidRPG/bin/AndroidRPG-debug-unaligned.apk : \
-/Volumes/Code/AndroidRPG/bin/AndroidRPG.ap_ \
-/Volumes/Code/AndroidRPG/bin/classes.dex \
-/Developer/android-sdk-macosx/tools/support/annotations.jar \
